8e5bd4e8a33aa64654a34ce281a584b6d9bf0d32
Shubham Sapra <shubham.sapra97@gmail.com>
2020-10-18 15:51:07 +0530

    added main method

---
+++ dice.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    # Getting user input
+    while True:
+        try:
+            N = int(input("Enter number of players "))
+            M = int(input("Enter points to accumulate "))
+        except ValueError:
+            print("Sorry, I didn't understand that.\n")
+            continue
+        else:
